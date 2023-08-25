920756ea1f15b8c2ca89d5686fe43552302bfa26
lance <lanceparker2001@gmail.com>
2023-08-26 00:53:03 +0530

    Trying to fix cookie transfer again, added cors directly in routerPath sigin/google/callback

--- routes/googleCallbackRouter.js
+++ routes/googleCallbackRouter.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { google } from "googleapis";
 import generateToken from "../utils/generateToken.js";
 import { User } from "../models/User.js";
+import cors from "cors";
 
 const router = express.Router();
 // ----------------------------------------------------------------
@@ -29,8 +30,86 @@ const refreshTokenCookieOptions = {
 // ----------------------------------------------------------------
 
 // Define your Google OAuth callback route
+router.get("/google/callback", async (req, res) => {
+  const authorizationCode = req.query.code;
+
+  // Make sure the token endpoint URL is correct
+  const tokenEndpoint = "https://oauth2.googleapis.com/token";
+
+  const tokenData = {
+    code: authorizationCode,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    redirect_uri: process.env.PROD_REDIRECT_URI,
+    grant_type: "authorization_code",
+  };
+
+  try {
+    // Make the token exchange request
+    const response = await axios.post(tokenEndpoint, null, {
+      params: tokenData,
+    });
+
+    // Getting user details
+    // const id_token = response.data.id_token;
+    const access_token = response.data.access_token;
+    const googleUser = jwt.decode(response.data.id_token);
+    // const googleUser = await getGoogleUser({ id_token, access_token });
+    // console.log(googleUser.name);
+    if (!googleUser.email_verified) {
+      res.status(400).json({
+        success: false,
+        message: "Google has not verified your account",
+      });
+    }
+
+    const accessToken = jwt.sign(
+      {
+        name: googleUser.name,
+        email: googleUser.email,
+        picture: googleUser.picture,
+        access_token: access_token,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1hr",
+      }
+    );
+    const refreshToken = jwt.sign(
+      {
+        name: googleUser.name,
+        email: googleUser.email,
+        picture: googleUser.picture,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "365d",
+      }
+    );
+
+    // console.log(access_token);
+    res.cookie("accessToken", accessToken, accessTokenCookieOptions);
+
+    res.cookie("refreshToken", refreshToken, refreshTokenCookieOptions);
+
+    //-------Create Session
+
+    //Working Youtube Code
+    oauth2Client.setCredentials({
+      access_token: access_token,
+    });
+    res.redirect(process.env.PROD_FRONTEND_URL);
+  } catch (error) {
+    console.error("Error exchanging authorization code:", error);
+    // Handle errors and send an appropriate response to the frontend
+    res
+      .status(500)
+      .send("Failed to authenticate user. Error exchanging authorization code");
+  }
+});
+
 router.get(
-  "/google/callback",
+  "/signin/google/callback",
   cors({
     origin: "*",
     credentials: true,
@@ -45,7 +124,7 @@ router.get(
       code: authorizationCode,
       client_id: process.env.CLIENT_ID,
       client_secret: process.env.CLIENT_SECRET,
-      redirect_uri: process.env.PROD_REDIRECT_URI,
+      redirect_uri: process.env.PROD_SIGNIN_REDIRECT_URI,
       grant_type: "authorization_code",
     };
 
@@ -55,11 +134,7 @@ router.get(
         params: tokenData,
       });
 
-      // Getting user details
-      // const id_token = response.data.id_token;
-      const access_token = response.data.access_token;
       const googleUser = jwt.decode(response.data.id_token);
-      // const googleUser = await getGoogleUser({ id_token, access_token });
       // console.log(googleUser.name);
       if (!googleUser.email_verified) {
         res.status(400).json({
@@ -68,117 +143,41 @@ router.get(
         });
       }
 
-      const accessToken = jwt.sign(
-        {
+      let user = await User.findOne({ email: googleUser.email });
+
+      if (!user) {
+        user = await User.create({
           name: googleUser.name,
           email: googleUser.email,
-          picture: googleUser.picture,
-          access_token: access_token,
-        },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: "1hr",
-        }
-      );
-      const refreshToken = jwt.sign(
-        {
-          name: googleUser.name,
-          email: googleUser.email,
-          picture: googleUser.picture,
-        },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: "365d",
-        }
-      );
+          pic: googleUser.picture,
+        });
 
-      // console.log(access_token);
-      res.cookie("accessToken", accessToken, accessTokenCookieOptions);
+        if (user) user.save();
+      }
 
-      res.cookie("refreshToken", refreshToken, refreshTokenCookieOptions);
+      const userDetail = {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        pic: user.pic,
+        token: generateToken(user._id),
+      };
 
-      //-------Create Session
-
-      //Working Youtube Code
-      oauth2Client.setCredentials({
-        access_token: access_token,
+      res.cookie("userLoginDetail", userDetail, {
+        maxAge: 86400000, // 24d
+        secure: true,
+        HttpOnly: true,
+        SameSite: "Lax",
       });
+      //
+
       res.redirect(process.env.PROD_FRONTEND_URL);
     } catch (error) {
-      console.error("Error exchanging authorization code:", error);
-      // Handle errors and send an appropriate response to the frontend
-      res
-        .status(500)
-        .send(
-          "Failed to authenticate user. Error exchanging authorization code"
-        );
+      console.error("Error Login in User", error);
+      res.status(500).send("Failed to Login user");
     }
   }
 );
-
-router.get("/signin/google/callback", async (req, res) => {
-  const authorizationCode = req.query.code;
-
-  // Make sure the token endpoint URL is correct
-  const tokenEndpoint = "https://oauth2.googleapis.com/token";
-
-  const tokenData = {
-    code: authorizationCode,
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    redirect_uri: process.env.PROD_SIGNIN_REDIRECT_URI,
-    grant_type: "authorization_code",
-  };
-
-  try {
-    // Make the token exchange request
-    const response = await axios.post(tokenEndpoint, null, {
-      params: tokenData,
-    });
-
-    const googleUser = jwt.decode(response.data.id_token);
-    // console.log(googleUser.name);
-    if (!googleUser.email_verified) {
-      res.status(400).json({
-        success: false,
-        message: "Google has not verified your account",
-      });
-    }
-
-    let user = await User.findOne({ email: googleUser.email });
-
-    if (!user) {
-      user = await User.create({
-        name: googleUser.name,
-        email: googleUser.email,
-        pic: googleUser.picture,
-      });
-
-      if (user) user.save();
-    }
-
-    const userDetail = {
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      pic: user.pic,
-      token: generateToken(user._id),
-    };
-
-    res.cookie("userLoginDetail", userDetail, {
-      maxAge: 86400000, // 24d
-      secure: true,
-      HttpOnly: true,
-      SameSite: "Lax",
-    });
-    //
-
-    res.redirect(process.env.PROD_FRONTEND_URL);
-  } catch (error) {
-    console.error("Error Login in User", error);
-    res.status(500).send("Failed to Login user");
-  }
-});
 
 export default router;
 export { oauth2Client };
