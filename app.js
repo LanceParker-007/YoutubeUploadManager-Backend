b9516e8355f00e408b732c92d74412e386384af0
lance <lanceparker2001@gmail.com>
2023-08-26 00:44:24 +0530

    Trying to fix cookie again, changed cors setting in app.js

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ config({
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express({ urlencoded: true }));
 app.use(cookieParser());
